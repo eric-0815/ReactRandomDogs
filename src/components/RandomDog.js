83fe133ca48edef0381ead0dcf94971ef7c1c904
erichong0815 <eric8.15@hotmail.com>
2021-09-21 05:29:30 +1000

    render push

--- src/components/RandomDog.js
+++ src/components/RandomDog.js
@@ -1,39 +1,60 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import axios from "axios";
 class RandomDog extends React.Component {
-	constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      url:"",
-    }
+    this.state = {
+      url: "",
+    };
+    this.pic = false;
+    this.video = false;
   }
-  componentDidMount(){
-    const _this=this;
+  componentDidMount() {
+    const _this = this;
     axios
-    .get("https://random.dog/woof.json")
-    .then(function (response) {
-      // handle success
-      console.log(response.data["url"]);
-      _this.setState({
-        url:response.data["url"]
+      .get("https://random.dog/woof.json")
+      .then(function (response) {
+        // handle success
+        console.log(response.data["url"]);
+        _this.setState({
+          url: response.data["url"],
+        });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
       });
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-
   }
 
   render() {
-      return (
-        <h1>{this.state.url}</h1>
-      )  
-  }
-}
+    let content;
+    var url = this.state.url;
+    var url_split = url.split(".");
+    var surrfix = url_split[url_split.length - 1];
+    surrfix = surrfix.toLowerCase();
+    if (surrfix == "jpeg" || surrfix == "jpg" || surrfix == "png") {
+      this.pic = true;
+      content = <img src={url} width="190" height="150"></img>;
+    }
 
+    return <div><div>{content}</div><div>{surrfix}</div></div>;
+  }
+  /*
+      <div>
+        <div>
+          {this.pic == true ? (
+            <img src={url} width="190" height="150"></img>
+          ) : (
+            "null"
+          )}
+        </div>
+        {url}
+      </div>
+      */
+}
+//<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">
+//
 export default RandomDog;
