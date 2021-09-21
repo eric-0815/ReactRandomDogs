cff6fb5965ec2fff03765fafc0b560e3bdc325e5
erichong0815 <eric8.15@hotmail.com>
2021-09-21 13:32:12 +1000

    background push

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import RenderDog from "./components/RandomDog/RenderDog";
 import Button_Click from "./components/Button/Button";
-import { Col, Container, Row } from "react-bootstrap";
+import Header from "./components/Header/Header";
 function App() {
   return (
-    <Container>
-      <Row>
-        <RenderDog />
-      </Row>
-      <Row>
-        <Button_Click />
-      </Row>
-    </Container>
+    <div style={{ 
+      backgroundImage: `url("https://images.pexels.com/photos/3198012/pexels-photo-3198012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260/")` 
+    }}>
+      <RenderDog />
+      <br/>
+      <Button_Click />
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.css";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+function Header(props) {
+  return (
+    <div></div>
+  );
+}
+export default Header;
+//https://images.pexels.com/photos/3198012/pexels-photo-3198012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260/100px270
