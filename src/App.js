2b458517e2073f16ed8c180955004a1c8ceef542
erichong0815 <eric8.15@hotmail.com>
2021-09-21 17:52:05 +1000

    Header Push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import RenderDog from "./components/RandomDog/RenderDog";
+<<<<<<< Updated upstream
 import Button_Click from "./components/Button/Button";
 import Footer from "./components/Footer/Footer";
 function App() {
@@ -15,6 +16,26 @@ function App() {
       <Button_Click />
       <br />
       <Footer />
+=======
+import ButtonClick from "./components/ButtonClick/ButtonClick";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <div
+        style={{
+          backgroundImage: `url("https://images.pexels.com/photos/3198012/pexels-photo-3198012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260/")`,
+        }}
+      >
+        <RenderDog />
+        <br />
+        <ButtonClick />
+        <br />
+        <Footer />
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
