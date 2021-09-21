dca4d086117c099de697bc3caefd10df4f596bc7
erichong0815 <eric8.15@hotmail.com>
2021-09-21 17:54:49 +1000

    changed files and folder names

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import RenderDog from "./components/RandomDog/RenderDog";
 import ButtonClick from "./components/ButtonClick/ButtonClick";
+<<<<<<< Updated upstream
 import Footer from "./components/Footer/Footer";
 function App() {
   return (
@@ -15,6 +16,25 @@ function App() {
       <ButtonClick />
       <br />
       <Footer />
+=======
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <div
+        style={{
+          backgroundImage: `url("https://images.pexels.com/photos/3198012/pexels-photo-3198012.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260/")`,
+        }}
+      >
+        <RenderDog />
+        <br />
+        <ButtonClick />
+        <br />
+        <Footer />
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/components/ButtonClick/ButtonClick.js
+++ src/components/ButtonClick/ButtonClick.js
@@ -1,8 +1,14 @@
 import "./ButtonClick.css";
+<<<<<<< Updated upstream:src/components/ButtonClick/ButtonClick.js
 import { Col, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 function ButtonClick() {
+=======
+import "bootstrap/dist/css/bootstrap.css";
+
+function ButtonClick(props) {
+>>>>>>> Stashed changes:src/components/Button/Button.js
   function refreshPage() {
     window.location.reload();
   }
